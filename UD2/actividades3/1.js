989607a1cda2cb84ee888595e44e7a3a4b449e5f
Robert Mosquera <robmosroc@alu.edu.gva.es>
2023-10-02 20:15:42 +0200

    Actividad 3

---
+++ UD2/actividades3/1.js
@@ -0,0 +1,5 @@
+let r1 = Math.random() * 256;
+let r2 = Math.random() * 256;
+let r3 = Math.random() * 256;
+
+document.addEventListener("DOMContentLoaded", () => {document.body.style.backgroundColor = "rgb(" +  r1 + "," + r2 + "," + r3 + ")"});
